dbca19d7028ffb629ebab4c3e0bfe204eb475745
Rexap17 <100739819+Rexap17@users.noreply.github.com>
2022-05-11 15:29:13 +0930

    Updated stats.cpp and stats.h

---
+++ stats.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+#include "stats.h"
+#include "player.h"
+#include "monster.h"
+
+using namespace std;
+
+Stats::Stats(){
+
+}
+
+void Stats::setName(){
+
+}
+
+void Stats::setHP(){
+    
+}
+
+void Stats::setATK(){
+
+}
+
+Stats::~Stats(){
+
+}
---
+++ stats.h
@@ -0,0 +1,24 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Stats{
+    public:
+        Stats();
+        //initialises these functions as virtual and they are inherited to the player and enemy classes
+        virtual void setName();
+        virtual void setHP();
+        virtual void setATK();
+
+        ~Stats();
+        //initialises variables that will be inherited to the child classes
+        string name;
+        int HP;
+        int ATK;
+
+};
+
+#endif
